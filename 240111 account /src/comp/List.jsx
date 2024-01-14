d0e2da40b7e184ba25eb7bae0652f85fc7e7002e
SSkkky <worte5633@gmail.com>
2024-01-14 17:50:56 +0900

    수습

--- 240111 account /src/comp/List.jsx
+++ 240111 account /src/comp/List.jsx
@@ -1,21 +1,31 @@
-import React from 'react';
+import React from "react";
 
-function List() {
-    return (
-        <div className="bank-list">
-            <ul>
-                {
-                    <li>
-                            <div className="left">
-                                <span className="date"></span>
-                                <p className="input-text"></p>
-                            </div>
-                            <p className="input-money"></p>
-                        </li>
-                }
-            </ul>
-        </div >
-    );
+function List({ data }) {
+  console.log(data); //들어옴
+  return (
+    <div className="bank-list">
+      <ul>
+        {data.map((obj) => {
+          return (
+            <li>
+              <div className="left">
+                <span className="date">{obj.date}</span>
+                <p className="input-text">{obj.txt}</p>
+              </div>
+              <p
+                className="input-money"
+                style={{
+                  color:
+                    obj.money < 0 ? "rgb(104, 114, 255);" : "rgb(255, 85, 127)",
+                }}
+              >
+                {obj.money.toLocaleString()}
+              </p>
+            </li>
+          );
+        })}
+      </ul>
+    </div>
+  );
 }
-
 export default List;
