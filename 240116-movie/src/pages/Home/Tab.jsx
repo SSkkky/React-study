87d809911d250a9d458954b66a3242e229d8ae10
SSkkky <worte5633@gmail.com>
2024-01-25 20:49:37 +0900

    메인 tab css 수정

--- 240116-movie/src/pages/Home/Tab.jsx
+++ 240116-movie/src/pages/Home/Tab.jsx
@@ -15,6 +15,7 @@ function Tab(props) {
 
     return (
         <div className={styles.mainTab}>
+          <div className='mainTab01'>
         <h3>인기상영작</h3>
         <Swiper
           slidesPerView={6.5}
@@ -31,6 +32,61 @@ function Tab(props) {
             })
         }
         </Swiper>
+          </div>
+          <div className='mainTab02'>
+        <h3>2번상영작</h3>
+        <Swiper
+          slidesPerView={6.5}
+          spaceBetween={10}
+          className={`mySwiper ${styles.swiper}`}
+        >
+          {
+            list.map((item) => {
+                return <SwiperSlide
+                key={item.id}
+                className={styles.swiperSlide}>
+                    <img src={thumb + '200/' + item.poster_path} alt="" />
+                </SwiperSlide>
+            })
+        }
+        </Swiper>
+          </div>
+          <div className='mainTab03'>
+        <h3>3번상영작</h3>
+        <Swiper
+          slidesPerView={6.5}
+          spaceBetween={10}
+          className={`mySwiper ${styles.swiper}`}
+        >
+          {
+            list.map((item) => {
+                return <SwiperSlide
+                key={item.id}
+                className={styles.swiperSlide}>
+                    <img src={thumb + '200/' + item.poster_path} alt="" />
+                </SwiperSlide>
+            })
+        }
+        </Swiper>
+          </div>
+          <div className='mainTab04'>
+        <h3>4번상영작</h3>
+        <Swiper
+          slidesPerView={6.5}
+          spaceBetween={10}
+          className={`mySwiper ${styles.swiper}`}
+        >
+          {
+            list.map((item) => {
+                return <SwiperSlide
+                key={item.id}
+                className={styles.swiperSlide}>
+                    <img src={thumb + '200/' + item.poster_path} alt="" />
+                </SwiperSlide>
+            })
+        }
+        </Swiper>
+          </div>
       </div>
     );
 }
