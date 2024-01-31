8eecde398a8f44629200919a111f90ce4c554225
SSkkky <worte5633@gmail.com>
2024-01-31 10:09:54 +0900

    add assets

--- 240116-movie/src/comp/Header.jsx
+++ 240116-movie/src/comp/Header.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { ReactComponent as BellIcon } from '../assets/bell.svg';
+import { ReactComponent as SearchIcon } from '../assets/search.svg';
 
 function Header(props) {
     return (<>
@@ -7,18 +9,18 @@ function Header(props) {
             <Link to='/'><h1>SKYFLIX</h1></Link>
             <nav>
                 <div className='left'>
-                <Link to='/'>HOME</Link>
-                <Link to='/'>SERISE</Link>
-                <Link to='/'>MOVIE</Link>
-                <Link to='/'>NEW</Link>
+                    <Link to='/'>HOME</Link>
+                    <Link to='/'>SERISE</Link>
+                    <Link to='/'>MOVIE</Link>
+                    <Link to='/'>NEW</Link>
                 </div>
                 <div className='right'>
-                    <Link to='/'></Link>
-                    <Link to='/'>☁</Link>
+                    <Link to='/'><SearchIcon width="25" heigth="25" stroke="#ffffff" /></Link>
+                    <Link to='/'><BellIcon width="20" heigth="20" fill="#ffffff" /></Link>
                 </div>
             </nav>
         </header>
-        </>
+    </>
     );
 }
 
--- 240116-movie/src/pages/Home/Main.jsx
+++ 240116-movie/src/pages/Home/Main.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { useEffect } from "react";
-import { useStore } from "../../Store";
+import { useStore } from '../../Store';
+import { ReactComponent as InfoIcon } from '../../assets/info.svg'
 
 // swiper
 import { Swiper, SwiperSlide } from 'swiper/react';
@@ -12,41 +13,41 @@ import '../../common.scss'
 import styles from '../../swiper.module.scss';
 
 function Main(props) {
-    const {getList, list, poster} = useStore();
-    useEffect(()=>{ getList() },[])
+    const { getList, list, poster } = useStore();
+    useEffect(() => { getList() }, [])
     console.log(list)
-    
+
     return (
         <Swiper
-        slidesPerView={1}
-        loop={true}
-        spaceBetween={0}
-        centeredSlides={true}
-        autoplay={{
-          delay: 5000,
-          disableOnInteraction: false,
-        }}
-        modules={[Autoplay]}
-        className={`mySwiper ${styles.swiper}`}>
-        {
-            list.map((item, i) => {
-                if(i < 5){
-                return <SwiperSlide key={item.id} className={styles.swiperSlide}
-                style={{backgroundImage:`url(${poster + item.backdrop_path})`}}>
-                    <div className={styles.mainContainer}>
-                        <strong className={styles.mainTitle}>{item.original_title}</strong>
-                        <p className={styles.mainOverview}>{item.overview}</p>
-                        <div className={styles.mainBtns}>
-                            <button className={styles.play}>
-                            </button>
-                            <button className={styles.info}>◎ Info</button>
-                        </div>
-                    </div>
-                </SwiperSlide>
-                }
-            })
-        }
-      </Swiper>
+            slidesPerView={1}
+            loop={true}
+            spaceBetween={0}
+            centeredSlides={true}
+            autoplay={{
+                delay: 5000,
+                disableOnInteraction: false,
+            }}
+            modules={[Autoplay]}
+            className={`mySwiper ${styles.swiper}`}>
+            {
+                list.map((item, i) => {
+                    if (i < 5) {
+                        return <SwiperSlide key={item.id} className={styles.swiperSlide}
+                            style={{ backgroundImage: `url(${poster + item.backdrop_path})` }}>
+                            <div className={styles.mainContainer}>
+                                <strong className={styles.mainTitle}>{item.original_title}</strong>
+                                <p className={styles.mainOverview}>{item.overview}</p>
+                                <div className={styles.mainBtns}>
+                                    <button className={styles.play}>
+                                    </button>
+                                    <button className={styles.info}><InfoIcon width="20" height="20" stroke="#ffffff" />Info</button>
+                                </div>
+                            </div>
+                        </SwiperSlide>
+                    }
+                })
+            }
+        </Swiper >
     );
 }
 
